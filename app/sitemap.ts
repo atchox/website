7a65906581ff41c5bb830deba11e7335287799c5
Atreya Choudhury <atreyachoudhury@hotmail.com>
2025-06-14 06:29:19 +0200

    sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,7 +14,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: post.lastmod || post.date,
     }))
 
-  const routes = ['', 'cv', 'about', 'teaching', 'teaching/LAII'].map((route) => ({
+  const routes = ['', 'cv', 'about', 'teaching', 'teaching/LAII', 'contact'].map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
