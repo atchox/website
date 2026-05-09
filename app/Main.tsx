161da9882353dcb30b9f06888439fcddca57fd34
Atreya Choudhury <atreyachoudhury@hotmail.com>
2026-05-09 17:08:54 +0200

    remove main page text

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,28 +11,22 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="relative left-1/2 mt-100 w-screen -translate-x-1/2 bg-red-950 sm:mt-110 md:mt-120 xl:mt-140">
-          <div className="mx-auto flex max-w-3xl flex-col items-start justify-between px-4 pb-15 sm:px-6 md:flex-row xl:max-w-5xl xl:px-0">
+          <div className="mx-auto flex max-w-3xl flex-col items-start justify-between px-4 pb-15 sm:px-6 xl:max-w-5xl xl:px-0">
             <Image
               src="/static/images/atreya_BW.jpg"
               alt="Portrait of Atreya at Stoos"
               height={400}
               width={400}
               title="Me at Stoos"
-              className="relative z-2 -mt-90 h-auto w-full sm:-mt-100 sm:w-4/5 md:-mt-70 md:w-2/5 lg:-mt-110 lg:w-3/5 xl:-mt-120 xl:w-1/2"
+              className="relative z-2 -mt-90 h-auto w-full sm:-mt-100 sm:w-3/5 md:-mt-70 md:w-2/5 lg:-mt-110 lg:w-3/5 xl:-mt-120 xl:w-1/2"
             />
-            <p className="mt-10 w-full text-right text-xl/8 text-gray-500 md:w-7/12 dark:text-gray-50">
-              My name is Atreya. I am a Statistics Masters student at ETH Zürich, interested in
-              non-parametric statistics and problems at the intersection of statistics and
-              bioinformatics.
-              <br />
-              <Link
-                href="/about"
-                className="keep-reading relative text-2xl/16 font-medium text-gray-500 no-underline! dark:text-gray-400"
-              >
-                About me
-                <span className="text-xl"> →</span>
-              </Link>
-            </p>
+            <Link
+              href="/about"
+              className="keep-reading relative text-2xl/16 font-medium text-gray-500 no-underline! dark:text-gray-400"
+            >
+              About me
+              <span className="text-xl"> →</span>
+            </Link>
           </div>
         </div>
         {/* <ul className="divide-y divide-gray-200 dark:divide-gray-700">
